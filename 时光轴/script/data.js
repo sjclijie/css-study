8c43aab431f3b9c794a8897d05cf94b658a3cd6d
Jaylee <sjclijie@vip.qq.com>
2014-12-24 00:33:30 +0800

    update

---
+++ 时光轴/script/data.js
@@ -0,0 +1,98 @@
+var data = [];
+ 
+data.push({
+    'date':'2014-03-31',
+    'intro':'今天很开心，找不到js代码',
+    'media':'<img src="../images/show03.jpg" width="370px">',
+    'like':302,
+    'connet':883
+})
+ 
+data.push({
+    'date':'2014-03-30',
+    'intro':'今天很开心，找不到js代码',
+    'media':'<img src="../images/show03.jpg" width="370px">',
+    'like':25838,
+    'connet':417
+})
+ 
+data.push({
+    'date':'2014-03-28',
+    'intro':'今天很开心，找不到js代码',
+    'media':'<img src="../images/show03.jpg" width="370px">',
+    'like':25838,
+    'connet':417
+})
+ 
+data.push({
+    'date':'2014-03-20',
+    'intro':'今天是20号哟，很开心，找不到js代码',
+    'media':'<img src="../images/show03.jpg" width="370px">',
+    'like':25838,
+    'connet':417
+})
+ 
+data.push({
+    'date':'2014-03-10',
+    'intro':'今天很开心，找不到js代码',
+    'media':'<img src="../images/show03.jpg" width="370px">',
+    'like':25838,
+    'connet':417
+})
+ 
+data.push({
+    'date':'2014-03-05',
+    'intro':'今天很开心，找不到js代码',
+    'media':'<img src="../images/show03.jpg" width="370px">',
+    'like':25838,
+    'connet':417
+})
+
+ 
+data.push({
+    'date':'2014-03-31',
+    'intro':'今天很开心，找不到js代码',
+    'media':'<img src="../images/show03.jpg" width="370px">',
+    'like':302,
+    'connet':883
+})
+ 
+data.push({
+    'date':'2013-06-30',
+    'intro':'今天很开心，找不到js代码',
+    'media':'<img src="../images/show03.jpg" width="370px">',
+    'like':25838,
+    'connet':417
+})
+ 
+data.push({
+    'date':'2012-02-28',
+    'intro':'今天很开心，找不到js代码',
+    'media':'<img src="../images/show03.jpg" width="370px">',
+    'like':25838,
+    'connet':417
+})
+ 
+data.push({
+    'date':'2015-02-20',
+    'intro':'今天是20号哟，很开心，找不到js代码',
+    'media':'<img src="../images/show03.jpg" width="370px">',
+    'like':25838,
+    'connet':417
+})
+ 
+data.push({
+    'date':'2012-06-10',
+    'intro':'今天很开心，找不到js代码',
+    'media':'<img src="../images/show03.jpg" width="370px">',
+    'like':25838,
+    'connet':417
+})
+ 
+data.push({
+    'date':'2013-05-05',
+    'intro':'今天很开心，找不到js代码',
+    'media':'<img src="../images/show03.jpg" width="370px">',
+    'like':25838,
+    'connet':417
+})
